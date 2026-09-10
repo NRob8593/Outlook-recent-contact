08641ba48d42c9028c84ac7dc1afa151a3dcba94
NRob8593 <nathan@circuit32.com>
2026-09-10 14:49:44 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Fill these in after registering an app in Azure AD (see README.md, step 1).
 const APP_CONFIG = {
-  clientId: "YOUR-AZURE-AD-APP-CLIENT-ID",
-  authority: "https://login.microsoftonline.com/YOUR-TENANT-ID-OR-common",
-  redirectUri: "https://YOUR-HOSTING-DOMAIN/taskpane.html",
+  clientId: "20a8e158-1958-45f1-be7d-e99853644e43",
+  authority: "https://login.microsoftonline.com/13b90261-d601-44aa-b579-2c61a59500b7",
+  redirectUri: "https://nrob8593.github.io/Outlook-recent-contact/taskpane.html",
 };
